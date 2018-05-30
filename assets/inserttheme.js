aa8232315b7c086901f18a136b528efcbd972502
AlexFlipnote <alexander.samuelsen.manso@gmail.com>
2018-05-30 18:41:23 +0200

    Better custom

--- assets/inserttheme.js
+++ assets/inserttheme.js
@@ -1,32 +1,15 @@
 // Simply insert that into the head
 var pluginURL = (link) => '<link href="'+link+'" type="text/css" rel="stylesheet">'
-document.querySelector('head').innerHTML += pluginURL("https://rawgit.com/AlexFlipnote/Discord_Theme/master/theme.css");
 
 chrome.storage.sync.get({
-  transparent: false,
-  transparentbackground: false,
-  blocked: false,
-  wideguilds: false
+  customcss: "",
+  discordtheme: true
 },function(items) {
-  console.log(items)
-  if (items.transparent == true) {
-    document.querySelector('head').innerHTML += pluginURL("https://rawgit.com/AlexFlipnote/Discord_Theme/master/plugins/transparent.css")
-    document.querySelector('head').innerHTML += `
-      <style>
-        .app { background: rgba(0, 0, 0, 0.75); }
-        body {
-          background: url(${items.transparentbackground});
-          background-size: cover; background-attachment: fixed; background-position: center; background-repeat: no-repeat;
-        }
-      </style>
-    `
+  if (items.discordtheme == true) {
+    document.querySelector('head').innerHTML += pluginURL("https://rawgit.com/AlexFlipnote/Discord_Theme/master/theme.css");
   }
 
-  if (items.blocked == true) {
-    document.querySelector('head').innerHTML += pluginURL("https://rawgit.com/AlexFlipnote/Discord_Theme/master/plugins/blocked.css")
-  }
-
-  if (items.wideguilds == true) {
-    document.querySelector('head').innerHTML += pluginURL("https://rawgit.com/AlexFlipnote/Discord_Theme/master/plugins/wideGuilds.css")
+  if (items.customcss.length > 2) {
+    document.querySelector('head').innerHTML += "<style>"+items.customcss+"</style>"
   }
 })
--- assets/options.js
+++ assets/options.js
@@ -1,22 +1,21 @@
 // Saves options to chrome.storage
 function save_options() {
-  var transparent = document.getElementById('transparent').checked;
-  var transparentbackground = document.getElementById('transparentbackground').value;
-  var blocked = document.getElementById('blocked').checked;
-  var wideguilds = document.getElementById('wideguilds').checked;
+  var customcss = document.getElementById('customcss').value;
+  var discordtheme = document.getElementById('discordtheme').checked;
 
   chrome.storage.sync.set({
-    transparent: transparent,
-    transparentbackground: transparentbackground,
-    blocked: blocked,
-    wideguilds: wideguilds
+    customcss: customcss,
+    discordtheme: discordtheme
   }, function() {
     // Update status to let user know options were saved.
-    var status = document.getElementById('status');
-    status.textContent = 'Options saved.';
+    var modal = document.getElementById('modal');
+    var modaltarget = document.getElementsByClassName('modal')[0];
+    modal.style.display = "block";
+    modaltarget.classList.remove('modal--close');
     setTimeout(function() {
-      status.textContent = '';
-    }, 750);
+      modaltarget.classList.add('modal--close');
+      setTimeout(function() { modal.style.display = "none"; }, 570);
+    }, 1000);
   });
 }
 
@@ -24,15 +23,11 @@ function save_options() {
 // stored in chrome.storage.
 function restore_options() {
   chrome.storage.sync.get({
-    transparent: false,
-    transparentbackground: false,
-    blocked: false,
-    wideguilds: false
+    customcss: "",
+    discordtheme: true
   }, function(items) {
-    document.getElementById('transparent').checked = items.transparent;
-    document.getElementById('transparentbackground').value = items.transparentbackground;
-    document.getElementById('blocked').checked = items.blocked;
-    document.getElementById('wideguilds').checked = items.wideguilds;
+    document.getElementById('customcss').value = items.customcss;
+    document.getElementById('discordtheme').checked = items.discordtheme;
   });
 }
 
